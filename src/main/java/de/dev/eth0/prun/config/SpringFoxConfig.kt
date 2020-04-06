4cb7d54fbd04990c29d1060dcaf216a461a1a1fb
deveth0 <amuthmann@dev-eth0.de>
2020-04-07 01:02:10 +0200

    Exclude spring error controller from swagger

--- src/main/java/de/dev/eth0/prun/config/SpringFoxConfig.kt
+++ src/main/java/de/dev/eth0/prun/config/SpringFoxConfig.kt
@@ -1,5 +1,6 @@
 package de.dev.eth0.prun.config
 
+import com.google.common.base.Predicates
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import springfox.documentation.builders.PathSelectors
@@ -16,7 +17,7 @@ open class SpringFoxConfig {
   open fun api(): Docket {
     return Docket(DocumentationType.SWAGGER_2)
         .select()
-        .apis(RequestHandlerSelectors.any())
+        .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
         .paths(PathSelectors.any())
         .build()
   }
